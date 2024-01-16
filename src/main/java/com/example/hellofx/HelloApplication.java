27a281ec4959ff9060daebe0f2bcc774198b817c
Lorenzo <lorenzo-a@live.fr>
2024-01-16 20:37:32 +0100

    Add title and background

--- src/main/java/com/example/hellofx/HelloApplication.java
+++ src/main/java/com/example/hellofx/HelloApplication.java
@@ -2,22 +2,29 @@ package com.example.hellofx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+//        Stage stage = new Stage();
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.AQUAMARINE);
+
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
