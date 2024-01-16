b06c908fae3b60e525f643e8ff39106bc1b13896
Lorenzo <lorenzo-a@live.fr>
2024-01-16 20:51:15 +0100

    Logo not working

--- src/main/java/com/example/hellofx/HelloApplication.java
+++ src/main/java/com/example/hellofx/HelloApplication.java
@@ -1,13 +1,15 @@
 package com.example.hellofx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.InputStream;
 
 public class HelloApplication extends Application {
 
@@ -18,12 +20,25 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
-//        Stage stage = new Stage();
         Group root = new Group();
         Scene scene = new Scene(root, Color.AQUAMARINE);
 
+        InputStream is = getClass().getResourceAsStream("/logo.png");
+        if (is == null) {
+            throw new RuntimeException("Cannot load image: /logo.png");
+        }
+
+        Image icon = new Image(is);
+        ImageView imageView = new ImageView(icon);
+        imageView.setFitHeight(32); // set the height to 32
+        imageView.setFitWidth(32); // set the width to 32
+        SnapshotParameters parameters = new SnapshotParameters();
+        parameters.setFill(Color.TRANSPARENT);
+        icon = imageView.snapshot(parameters, null);
+
+        stage.getIcons().add(icon);
         stage.setTitle("Hello!");
+
         stage.setScene(scene);
         stage.show();
     }
