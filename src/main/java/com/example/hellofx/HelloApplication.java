18e2f31859eae76b6910d538628aca8e6ca94932
Lorenzo <lorenzo-a@live.fr>
2024-01-17 18:05:44 +0100

    Setup project

--- src/main/java/com/example/hellofx/HelloApplication.java
+++ src/main/java/com/example/hellofx/HelloApplication.java
@@ -1,31 +1,12 @@
 package com.example.hellofx;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-/*
-    StageStyle :
-    DECORATED - The Stage will have a border and other decorations typical of a window, depending on the platform.
-    UNDECORATED - The Stage will have no decorations.
-    TRANSPARENT - The Stage will have no decorations, and the pixels of its window will be fully transparent.
-*/
 
 public class HelloApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
-//        primaryStage.isFullScreen();
-//        primaryStage.isAlwaysOnTop();
-
-        Color black = Color.BLUE;
-        Scene scene = new Scene(new Group(), 1280, 720, black);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Hello FX");
 
         primaryStage.show();
     }
