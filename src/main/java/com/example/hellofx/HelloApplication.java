f96bbef837daf4a74166d5aaf60635788075848b
Lorenzo <lorenzo-a@live.fr>
2024-01-17 17:28:46 +0100

    initStyle

--- src/main/java/com/example/hellofx/HelloApplication.java
+++ src/main/java/com/example/hellofx/HelloApplication.java
@@ -2,11 +2,22 @@ package com.example.hellofx;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/*
+    StageStyle :
+    DECORATED - The Stage will have a border and other decorations typical of a window, depending on the platform.
+    UNDECORATED - The Stage will have no decorations.
+    TRANSPARENT - The Stage will have no decorations, and the pixels of its window will be fully transparent.
+*/
 
 public class HelloApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+//        primaryStage.initStyle(StageStyle.DECORATED);
+//        primaryStage.isFullScreen();
+//        primaryStage.isAlwaysOnTop();
         primaryStage.show();
     }
 
