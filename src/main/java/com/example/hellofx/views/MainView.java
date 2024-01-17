72b5b7d966c223979dbb7f24d12e5a17758f0adb
Lorenzo <lorenzo-a@live.fr>
2024-01-17 18:54:20 +0100

    Setup text field

--- src/main/java/com/example/hellofx/views/MainView.java
+++ src/main/java/com/example/hellofx/views/MainView.java
@@ -2,6 +2,9 @@ package com.example.hellofx.views;
 
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class MainView extends VBox {
@@ -9,7 +12,11 @@ public class MainView extends VBox {
             super(spacing);
 
             ObservableList<Node> components = this.getChildren();
-            
-            components.addAll();
+
+            Label someLabel = new Label("Un champ de texte :");
+            TextField someTextField = new TextField();
+            PasswordField somePasswordField = new PasswordField();
+
+            components.addAll(someLabel, someTextField, somePasswordField);
         }
 }
