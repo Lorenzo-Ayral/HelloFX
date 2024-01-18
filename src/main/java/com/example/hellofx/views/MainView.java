b22164595e40cf4d6a3a1e96c826aeb4529e08e7
Lorenzo <lorenzo-a@live.fr>
2024-01-18 19:47:09 +0100

    Add button

--- src/main/java/com/example/hellofx/views/MainView.java
+++ src/main/java/com/example/hellofx/views/MainView.java
@@ -16,36 +16,21 @@ import java.util.List;
 public class MainView extends VBox {
         public MainView(double spacing) {
             super(spacing);
-
             ObservableList<Node> components = this.getChildren();
 
-            Label someLabel = new Label("Un champ de texte :");
-            TextField someTextField = new TextField();
-            PasswordField somePasswordField = new PasswordField();
-            Separator separator = new Separator(Orientation.HORIZONTAL);
-            List<String> fontFamilies = Font.getFamilies();
-            if (fontFamilies.contains("Tlwg Mono")) {
-                System.out.println("La police 'Tlwg Mono' est disponible.");
-            } else {
-                System.out.println("La police 'Tlwg Mono' n'est pas disponible.");
-            }
-            
-            TextArea someTextArea = new TextArea();
-            someTextArea.setWrapText(true);
+            Button someButton = new Button("Click me!");
+            someButton.setOnAction(e -> {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Look, an Information Dialog");
+                alert.setContentText("I have a great message for you!");
+                alert.showAndWait();
+            });
 
-//            Font someFont = new Font("Tlwg Mono", 30);
-//
-//            for(String s : Font.getFontNames())
-//                System.out.println(s);
+            Button someOtherButton = new Button("Click me!");
+            someOtherButton.setDefaultButton(true);
 
 
-            Text someText = new Text("Un texte\nsur plusieurs\nlignes");
-//            Font.font(<family>, <weight>, <posture>, <size>)
-            someText.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.ITALIC, 30));
-            someText.setFill(Color.RED);
-            someText.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
-            someText.setLineSpacing(10);
-
-            components.addAll(someLabel, someTextField, separator, someTextArea, somePasswordField, someText);
+            components.addAll(someButton, someOtherButton);
         }
 }
