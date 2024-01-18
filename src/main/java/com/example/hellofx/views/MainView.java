59685f7c9dfa0a9936d2b2713fa35a305f25a591
Lorenzo <lorenzo-a@live.fr>
2024-01-18 19:29:48 +0100

    Check font

--- src/main/java/com/example/hellofx/views/MainView.java
+++ src/main/java/com/example/hellofx/views/MainView.java
@@ -8,6 +8,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import java.util.List;
+
 public class MainView extends VBox {
         public MainView(double spacing) {
             super(spacing);
@@ -18,11 +20,20 @@ public class MainView extends VBox {
             TextField someTextField = new TextField();
             PasswordField somePasswordField = new PasswordField();
             Separator separator = new Separator(Orientation.HORIZONTAL);
+            List<String> fontFamilies = Font.getFamilies();
+            if (fontFamilies.contains("AnjaliOldLipi")) {
+                System.out.println("La police 'AnjaliOldLipi' est disponible.");
+            } else {
+                System.out.println("La police 'AnjaliOldLipi' n'est pas disponible.");
+            }
             
             TextArea someTextArea = new TextArea();
             someTextArea.setWrapText(true);
 
-            Font someFont = new Font("Stencil", 50);
+            Font someFont = new Font("AnjaliOldLipi", 50);
+
+            for(String s : Font.getFamilies())
+                System.out.println(s);
 
             Text someText = new Text("Un texte\nsur plusieurs\nlignes");
             someText.setFont(someFont);
