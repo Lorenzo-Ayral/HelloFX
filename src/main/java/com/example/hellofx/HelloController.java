3f1a8d449a79116176aaacf83c286e66b01e1fbe
Lorenzo <lorenzo-a@live.fr>
2024-01-20 20:50:55 +0100

    add buttons

--- src/main/java/com/example/hellofx/HelloController.java
+++ src/main/java/com/example/hellofx/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.hellofx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void up(ActionEvent actionEvent) {
+        System.out.println("up");
+    }
+    public void down(ActionEvent actionEvent) {
+        System.out.println("down");
+    }
+    public void right(ActionEvent actionEvent) {
+        System.out.println("right");
+    }
+    public void left(ActionEvent actionEvent) {
+        System.out.println("left");
     }
 }
