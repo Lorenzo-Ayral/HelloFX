8a458bc10e346b69788c41a2d5df7f21f71aad76
Lorenzo <lorenzo-a@live.fr>
2024-01-20 21:08:38 +0100

    add buttons action

--- src/main/java/com/example/hellofx/HelloController.java
+++ src/main/java/com/example/hellofx/HelloController.java
@@ -3,18 +3,25 @@ package com.example.hellofx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
+    @FXML
+    private Circle circle;
+    private double x;
+    private double y;
+
+
     public void up(ActionEvent actionEvent) {
-        System.out.println("up");
+        circle.setCenterY(y-=1);
     }
     public void down(ActionEvent actionEvent) {
-        System.out.println("down");
+        circle.setCenterY(y+=1);
     }
     public void right(ActionEvent actionEvent) {
-        System.out.println("right");
+        circle.setCenterX(x+=1);
     }
     public void left(ActionEvent actionEvent) {
-        System.out.println("left");
+        circle.setCenterX(x -= 1);
     }
 }
